de5da3a13582c13ffe375b42b13e47d57f22761e
you-mo77 <86389597+you-mo77@users.noreply.github.com>
2023-11-08 20:35:51 +0900

    全帯域のフィルタリング完了　バッファ間でノイズ(不連続だから？)

--- work.py
+++ work.py
@@ -6,13 +6,16 @@ import time
 from scipy import signal
 
 #音声取得
-data, fs = lib.load("crystalized.short.wav",mono=False, sr=48000)
+data, fs = lib.load("crystalized_short.wav",mono=False, sr=48000)
 #print(f"data:{data.dtype}")
 #data = data.astype(np.float64)
 #print(f"data2:{data.dtype}")
 
 
-total = np.zeros((2,0))
+total1 = np.zeros((2,0))
+total2 = np.zeros((2,0))
+total3 = np.zeros((2,0))
+
 """
 n = np.array([
     [1, 2, 3, 4, 5],
@@ -31,7 +34,7 @@ start_pos = 0
 
 #コールバック関数
 def callback(frame_count,h,fc):
-    global total
+    global total1,total2,total3
     global start_pos, data, fs
     
     # 切り出す(ステレオ->2行frame_count列　)
@@ -40,30 +43,42 @@ def callback(frame_count,h,fc):
 
     #フィルタリング
     #filtered_data = np.zeros((2, (int(h[0].shape[0]) + int(output_data[0].shape[0]) - 1)))
-    filtered_data = np.zeros(output_data.shape)
+    filtered_data1 = np.zeros(output_data.shape)
+    filtered_data2 = np.zeros(output_data.shape)
+    filtered_data3 = np.zeros(output_data.shape)
+
     if output_data.shape[1] != 0:
-        filtered_data[0] = filter0(fc,output_data[0])
-        filtered_data[1] = filter0(fc,output_data[1])
+        filtered_data1[0] = filter1(fc,output_data[0])
+        filtered_data1[1] = filter1(fc,output_data[1])
+        filtered_data2[0] = filter2(fc,output_data[0])
+        filtered_data2[1] = filter2(fc,output_data[1])        
+        filtered_data3[0] = filter3(fc,output_data[0])
+        filtered_data3[1] = filter3(fc,output_data[1])
+
     #print(f"filtered_data1:{filtered_data.dtype}")
-    filtered_data = filtered_data.astype(np.float32)
+    filtered_data1 = filtered_data1.astype(np.float32)
+    filtered_data2 = filtered_data2.astype(np.float32)
+    filtered_data3 = filtered_data3.astype(np.float32)
     #print(f"filtered_data2:{filtered_data.dtype}")
 
 
-    total = np.append(total,filtered_data, axis=1)
+    total1 = np.append(total1,filtered_data1, axis=1)
+    total2 = np.append(total2,filtered_data2, axis=1)
+    total3 = np.append(total3,filtered_data3, axis=1)
 
-    #sf.write("in_callback.wav",total.T,fs,format="wav")
+    #sf.write("in_callback.wav",total1.T,fs,format="wav")
     #print(type(filtered_data))
-    print(f"filtered_data:{filtered_data.dtype}")
+    #print(f"filtered_data:{filtered_data.dtype}")
 
     # 転置(行と列入れ替え[左1, 右1],[左2, 右2],・・・)
-    filtered_data = filtered_data.T
+    filtered_data1 = filtered_data1.T
 
     #byte形式に直すのがうまくいってない(フィルタ後の音声はきれいに聞こえた) 
     # １行に([左1, 右1, 左2, 右2, ・・・])
-    filtered_data = np.ravel(filtered_data)
+    filtered_data1 = np.ravel(filtered_data1)
 
     #バイト形式に変換
-    byte_data = filtered_data.tobytes('C')
+    byte_data = filtered_data1.tobytes('C')
 
     #print("before ravel: ", output_data.shape)
     #切り出し位置更新
@@ -76,8 +91,9 @@ def callback(frame_count,h,fc):
 
     #再生
     return (byte_data, pa.paContinue)
-
+    #return(b'',pa.paContinue)
 #フィルタ関数
+"""
 def filtering0(h: np.ndarray, data: np.ndarray):
 
     #フィルタ適用
@@ -100,13 +116,32 @@ def filtering2(h: np.ndarray, data: np.ndarray, range_num: int):
     filtered2 = np.convolve(data,h[2])
     
     return filtered2
-
+"""
+    
 #新フィルタ関数
-def filter0(fc, data:np.ndarray):
+def filter1(fc, data:np.ndarray):
+    
+    nyquist_freq = 0.5 * fs
+    normal_cutoff = fc[1] / nyquist_freq
+    b, a = signal.butter(7, normal_cutoff, btype='low')
+    filtered_signal = signal.filtfilt(b, a, data)
+
+    return filtered_signal
+def filter2(fc, data:np.ndarray):
     
     nyquist_freq = 0.5 * fs
     normal_cutoff = fc[2] / nyquist_freq
     b, a = signal.butter(7, normal_cutoff, btype='low')
+    filtered_signal = signal.filtfilt(b, a, data)
+    normal_cutoff = fc[1] / nyquist_freq
+    b, a = signal.butter(7, normal_cutoff, btype='high')
+    filtered_signal = signal.filtfilt(b, a, filtered_signal)
+
+    return filtered_signal
+def filter3(fc, data:np.ndarray):
+    nyquist_freq = 0.5 * fs
+    normal_cutoff = fc[2] / nyquist_freq
+    b, a = signal.butter(7, normal_cutoff, btype='high')
     filtered_signal = signal.filtfilt(b, a, data)
 
     return filtered_signal
@@ -157,14 +192,16 @@ def main():
                     rate=fs,
                     output=True,
                     stream_callback=lambda a,b,c,d:callback(b,h ,fc),
-                    frames_per_buffer=11857608)
+                    frames_per_buffer=48000)
 
     while stream.is_active():
         time.sleep(0.1)
     
     print("terminated")
-    #print(total.shape)
-    sf.write("output_new.wav",total.T,fs,format="wav")
+    #print(total1.shape)
+    sf.write("output1.wav",total1.T,fs,format="wav")
+    sf.write("output2.wav",total2.T,fs,format="wav")
+    sf.write("output3.wav",total3.T,fs,format="wav")
 
 
     stream.close()
